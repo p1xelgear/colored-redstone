c89642391abe513e2bdc34abe5f37ff732b34263
Kamil Gronowski <kamil540@gazeta.pl>
2018-11-12 20:22:47 +0100

    Different pickup delay for item thrown and from block break

--- src/main/java/pyre/coloredredstone/items/IColoredItem.java
+++ src/main/java/pyre/coloredredstone/items/IColoredItem.java
@@ -7,11 +7,13 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import pyre.coloredredstone.ColoredRedstone;
 import pyre.coloredredstone.entities.EntityItemColored;
 import pyre.coloredredstone.util.EnumColor;
 import pyre.coloredredstone.util.Reference;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -63,9 +65,15 @@ public interface IColoredItem {
         newEntityItem.motionX = oldEntityItem.motionX;
         newEntityItem.motionY = oldEntityItem.motionY;
         newEntityItem.motionZ = oldEntityItem.motionZ;
-        newEntityItem.setPickupDelay(40);
         newEntityItem.hoverStart = ((EntityItem) oldEntityItem).hoverStart;
         newEntityItem.lifespan = ((EntityItem) oldEntityItem).lifespan;
+        try {
+            Field field = ReflectionHelper.findField(EntityItem.class, "pickupDelay", "field_145804_b");
+            newEntityItem.setPickupDelay((Integer) field.get(oldEntityItem));
+        } catch (IllegalAccessException  e) {
+            ColoredRedstone.logger.error("Cannot get 'pickupDelay' value for ColoredEntityItem.", e);
+            newEntityItem.setDefaultPickupDelay();
+        }
 
         return newEntityItem;
     }
